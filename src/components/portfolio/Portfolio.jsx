706dec662662ff373b039f51331ed4f37f80eaf4
Pranjal0981 <pkshukla988@gmail.com>
2024-06-06 23:11:11 +0530

    Update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,122 +9,95 @@ const TeamMember = () => {
       </div>
 
       <div className="card_Container">
-        <div className="card">
-          <div className="imbBx">
-            <img src="./vaidruti_mishra.jpeg" alt="" />
-          </div>
-          <div className="content">
-            <div className="contentBx">
-              <h1>
-                Adv. Vaidruti Mishra <br />
-                <span>Advocate On Record
-                  Supreme Court Of India
-                  Expertises- In Coporate settlements , Family Law,Criminal Cases, Civil Cases and Dispute Resolution matters and Arbitration cases.</span>
-              </h1>
+        <div className="card-con">
+          <div className="card">
+            <div className="imbBx">
+              <img src="./vaidruti_mishra.jpeg" alt="Vaidruti Mishra" />
             </div>
-            <ul className="sci">
-              <li style={{ '--i': 1 }}>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-              </li>
-              <li style={{ '--i': 2 }}>
-                <a href="#"><i className="fab fa-github"></i></a>
-              </li>
-              <li style={{ '--i': 3 }}>
-                <a href="#"><i className="fab fa-linkedin-in"></i></a>
-              </li>
-            </ul>
+            <div className="content">
+              <div className="contentBx">
+                <h1>
+                  Adv. Vaidruti Mishra <br />
+                  <span>Advocate On Record
+                    Supreme Court Of India
+                    Expertises- In Coporate settlements , Family Law, Criminal Cases, Civil Cases and Dispute Resolution matters and Arbitration cases.</span>
+                </h1>
+              </div>
+              <ul className="sci">
+                <li style={{ '--i': 1 }}>
+                  <a href="#"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li style={{ '--i': 2 }}>
+                  <a href="#"><i className="fab fa-github"></i></a>
+                </li>
+                <li style={{ '--i': 3 }}>
+                  <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="card">
+            <div className="imbBx">
+              <img src="./ashwain_mishra.jpeg" alt="Ashwain Mishra" />
+            </div>
+            <div className="content">
+              <div className="contentBx">
+                <h1>
+                  Adv. Ashiwan Mishra <br />
+                  <span>Expertise -Government Matters, family law, criminal law Practices In Supreme Court Of India as well as all the Courts all over India</span>
+                </h1>
+              </div>
+              <ul className="sci">
+                <li style={{ '--i': 1 }}>
+                  <a href="#"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li style={{ '--i': 2 }}>
+                  <a href="#"><i className="fab fa-github"></i></a>
+                </li>
+                <li style={{ '--i': 3 }}>
+                  <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="card">
+            <div className="imbBx">
+              <img src="./amit_kumar.jpeg" alt="Amit Kumar" />
+
+            </div>
+            <div className="content">
+              <div className="contentBx">
+                <h1>
+                  Adv. Amit Kumar <br />
+                  <span>Expertise -Civil law, family law, criminal law Practices In High Courts and district courts all over India</span>
+                </h1>
+              </div>
+              <ul className="sci">
+                <li style={{ '--i': 1 }}>
+                  <a href="#"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li style={{ '--i': 2 }}>
+                  <a href="#"><i className="fab fa-github"></i></a>
+                </li>
+                <li style={{ '--i': 3 }}>
+                  <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                </li>
+              </ul>
+            </div>
+          
           </div>
         </div>
-        <div className="card">
+        
+<div className="card-con">
+  <div className="card">
           <div className="imbBx">
-            <img src="./ashwain_mishra.jpeg" alt="" />
-          </div>
-          <div className="content">
-            <div className="contentBx">
-              <h1>
-              Adv.  Ashiwan mishra <br />
-                <span>
-                  Expertise -Government Matters , family law , criminal law Pratices In  Supreme Court Of India as well as all the Courts all over India</span>
-              </h1>
-            </div>
-            <ul className="sci">
-              <li style={{ '--i': 1 }}>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-              </li>
-              <li style={{ '--i': 2 }}>
-                <a href="#"><i className="fab fa-github"></i></a>
-              </li>
-              <li style={{ '--i': 3 }}>
-                <a href="#"><i className="fab fa-linkedin-in"></i></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-       
-
-        <div className="card">
-          <div className="imbBx">
-            <img src="./rakesh_soni.jpeg" alt="" />
-          </div>
-          <div className="content">
-            <div className="contentBx">
-              <h1>
-             Adv.  Rakesh Soni <br />
-                <span>Expertise -Dispute settlement expert and financial law Practices in all courts
-</span>
-              </h1>
-            </div>
-            <ul className="sci">
-              <li style={{ '--i': 1 }}>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-              </li>
-              <li style={{ '--i': 2 }}>
-                <a href="#"><i className="fab fa-github"></i></a>
-              </li>
-              <li style={{ '--i': 3 }}>
-                <a href="#"><i className="fab fa-linkedin-in"></i></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="imbBx">
-            <img src="./sharad_rai.jpeg" alt="" />
+            <img src="./sharad_rai.jpeg" alt="Sharad Rai" />
           </div>
           <div className="content">
             <div className="contentBx">
               <h1>
                 Adv. Sharad Rai <br />
-                <span>Expertise -Coporate law and financial law as well as Taxation Matters Practice in all the tribunals and Distict Courts</span>
-              </h1>
-            </div>
-            <ul className="sci">
-              <li style={{ '--i': 1 }}>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-              </li>
-              <li style={{ '--i': 2 }}>
-                <a href="#"><i className="fab fa-github"></i></a>
-              </li>
-              <li style={{ '--i': 3 }}>
-                <a href="#"><i className="fab fa-linkedin-in"></i></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="imbBx">
-            <img src="./slide9.jpeg" alt="" />
-          </div>
-          <div className="content">
-            <div className="contentBx">
-              <h1>
-                Adv. Ritu Renewal
- <br />
-                <span>Expertise -coporate law and financial  law
-                  Practice  in both courts</span>
+                <span>Expertise -Corporate law and financial law as well as Taxation Matters Practice in all the tribunals and District Courts</span>
               </h1>
             </div>
             <ul className="sci">
@@ -142,14 +115,13 @@ const TeamMember = () => {
         </div>
         <div className="card">
           <div className="imbBx">
-            <img src="./amit_kumar.jpeg" alt="" />
+            <img src="./slide9.jpeg" alt="Ritu Renewal" />
           </div>
           <div className="content">
             <div className="contentBx">
               <h1>
-                Adv. Amit Kumar
-                <br />
-                <span>Expertise -Civil law , family law , criminal law Pratices In  High Courts and district courts   all over India</span>
+                Adv. Ritu Renewal <br />
+                <span>Expertise -Corporate law and financial law Practice in both courts</span>
               </h1>
             </div>
             <ul className="sci">
@@ -165,6 +137,32 @@ const TeamMember = () => {
             </ul>
           </div>
         </div>
+        <div className="card">
+          <div className="imbBx">
+              <img src="./rakesh_soni.jpeg" alt="Rakesh Soni" />
+          </div>
+            <div className="content">
+              <div className="contentBx">
+                <h1>
+                  Adv. Rakesh Soni <br />
+                  <span>Expertise -Dispute settlement expert and financial law Practices in all courts</span>
+                </h1>
+              </div>
+              <ul className="sci">
+                <li style={{ '--i': 1 }}>
+                  <a href="#"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li style={{ '--i': 2 }}>
+                  <a href="#"><i className="fab fa-github"></i></a>
+                </li>
+                <li style={{ '--i': 3 }}>
+                  <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                </li>
+              </ul>
+            </div>
+        </div>
+</div>
+      
       </div>
     </div>
   );
--- src/components/whatwedid/Work.jsx
+++ src/components/whatwedid/Work.jsx
@@ -1,48 +1,56 @@
-import { useEffect } from "react";
-import "./work.scss";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import './work.scss';
 
 const Work = () => {
     const containerAnimation = useAnimation();
-    const { ref, inView } = useInView({ triggerOnce: true });
- 
+    const { ref } = useInView({ triggerOnce: true });
+
     useEffect(() => {
-        if (inView) {
             containerAnimation.start({
                 opacity: 1,
                 y: 0,
                 transition: {
-                    staggerChildren: 0.1, // Stagger the appearance of each box
+                    staggerChildren: 0.05, // Adjust the stagger children timing
                 },
             });
-        }
-    }, [inView, containerAnimation]);
-    const handleClick=()=>{
+        
+    }, [containerAnimation]);
 
-        const contact = document.getElementById("Contact")
-        contact.scrollIntoView({ behavior: 'smooth' })
-    }
+    const handleClick = () => {
+        const contact = document.getElementById('Contact');
+    };
 
     return (
         <>
             <div ref={ref} className="listContainer">
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2> Civil Cases</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Civil Cases</h2>
                     <p>
-
                         Property disputes: Conflicts over property ownership or possession.
                         Contract disputes: Disagreements over the terms or performance of a contract.
                         Torts: Cases involving harm caused by one party to another, such as negligence or defamation.
-Family matters: Issues like divorce, child custody, and maintenance.    </p> 
-               <button onClick={handleClick}>Consult Now</button>
+                        Family matters: Issues like divorce, child custody, and maintenance.
+                    </p>
+                    <button onClick={handleClick}>Consult Now</button>
                 </motion.div>
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2>Criminal Cases
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Criminal Cases</h2>
                     <p>
                         Criminal cases involve actions that are considered offenses against the state or public.
-
                         Offenses against persons: Such as murder, assault, and kidnapping.
                         Offenses against property: Such as theft, burglary, and vandalism.
                         White-collar crimes: Such as fraud, embezzlement, and bribery.
@@ -50,12 +58,16 @@ Family matters: Issues like divorce, child custody, and maintenance.    </p>
                     </p>
                     <button onClick={handleClick}>Get Legal Help</button>
                 </motion.div>
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2> Constitutional Cases
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Constitutional Cases</h2>
                     <p>
                         These cases involve interpretation and application of the Constitution of India.
-
                         Fundamental rights:
                         Cases related to the violation of rights guaranteed under the Constitution.
                         Federal disputes:
@@ -65,61 +77,84 @@ Family matters: Issues like divorce, child custody, and maintenance.    </p>
                     </p>
                     <button onClick={handleClick}>Learn More</button>
                 </motion.div>
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2>Administrative Cases
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Administrative Cases</h2>
                     <p>
                         Administrative cases involve disputes over decisions made by government agencies or bodies.
-
                         Service matters: Disputes involving government employees and their employment conditions.
-Regulatory issues: Challenges to regulations or administrative decisions affecting individuals or businesses.                    </p>
+                        Regulatory issues: Challenges to regulations or administrative decisions affecting individuals or businesses.
+                    </p>
                     <button onClick={handleClick}>Consultation</button>
                 </motion.div>
-
             </div>
             <div ref={ref} className="listContainer">
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2>Family Cases
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Family Cases</h2>
                     <p>
                         Family law cases deal with matters related to family relationships.
-
                         Marriage and divorce: Legal dissolution of marriage and related issues like alimony.
                         Child custody and support: Determination of custody and financial support for children.
                         Adoption: Legal process of adopting a child
                     </p>
                     <button onClick={handleClick}>Consult Now</button>
                 </motion.div>
-            
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2> Environmental Cases
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>Environmental Cases</h2>
                     <p>
-                        These cases address issues related to environmental protection and conservation. :
-
+                        These cases address issues related to environmental protection and conservation.
                         Pollution control: Disputes over pollution and its impact on health and the environment.
-Conservation efforts: Cases related to the preservation of natural resources and wildlife.                    </p>
+                        Conservation efforts: Cases related to the preservation of natural resources and wildlife.
+                    </p>
                     <button onClick={handleClick}>Learn More</button>
                 </motion.div>
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
-                    <h2>The Arbitration and Conciliation
-</h2>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
+                    <h2>The Arbitration and Conciliation</h2>
                     <p>
                         Arbitration and conciliation are effective ADR mechanisms that offer parties a flexible, efficient, and confidential way to resolve disputes outside the traditional court system.
-                        The Arbitration and Conciliation Act, 1996 provides the legal foundation for both arbitration and conciliation in India. 
+                        The Arbitration and Conciliation Act, 1996 provides the legal foundation for both arbitration and conciliation in India.
                     </p>
                     <button onClick={handleClick}>Consultation</button>
                 </motion.div>
-                <motion.div className="box" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0, y: 50 }} animate={containerAnimation}>
+                <motion.div
+                    className="box"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={containerAnimation}
+                >
                     <h2>Experience</h2>
                     <p>
-With over more than five decades of combined experience across various legal domains, our seasoned lawyers are well-equipped to handle your case with precision and efficacy. You can trust in our expertise to navigate through the complexities of the legal system and ensure that your interests are safeguarded. Let us put our knowledge and skills to work for you, providing you with the representation you deserve.                    </p>
+                        With over more than five decades of combined experience across various legal domains, our seasoned lawyers are well-equipped to handle your case with precision and efficacy. You can trust in our expertise to navigate through the complexities of the legal system and ensure that your interests are safeguarded. Let us put our knowledge and skills to work for you, providing you with the representation you deserve.
+                    </p>
                     <button onClick={handleClick}>Get Legal Help</button>
                 </motion.div>
             </div>
         </>
-      
     );
-}
+};
 
 export default Work;
